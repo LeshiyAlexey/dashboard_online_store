8da2faa246d2745d884e27275748a8de934bea74
Alexey Edakin <lesha.edakin@gmail.com>
2026-08-25 15:30:14 +0300

    Update data_merge.py

--- src/data_merge.py
+++ src/data_merge.py
@@ -9,5 +9,7 @@ def merge_all_data(items_data, orders_data, users_data):
     """
     data_merged = orders_data.merge(users_data, on='user_id', how='left')
     merged_final = data_merged.merge(items_data, on='item_id', how='left')
-    print(f"Всего записей в обьединеной таблице: {len(merged_final)} записей")
+    print(f"")
+    print(f"Данные обьединены")
+    print(f"Всего записей в обьединеной таблице: {len(merged_final)}")
     return merged_final
